e1f89f2d0d69b50d20f936ca146a82c0eb35ac6f
punyamsingh <89277920+punyamsingh@users.noreply.github.com>
2023-06-01 01:46:39 +0530

    All fixes till video 21 (#2)
    i am squashing and merging

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,14 +4,12 @@ import React, { useState } from 'react'
 export default function TextForm(props) {
 
     const handleUpClick = () => {
-        // console.log("Uppercase was clicked\n" + text);
         let newText = text.toUpperCase();
         setText(newText);
         props.showAlert("converted to uppercase!", "success")
     }
 
     const handleLowClick = () => {
-        // console.log("Lowercase was clicked\n" + text);
         let newText = text.toLowerCase();
         setText(newText);
         props.showAlert("converted to lowercase!", "success")
@@ -22,32 +20,33 @@ export default function TextForm(props) {
         props.showAlert("cleared text!", "success")
     }
 
-
-
     const handleMostFreqWord = () => {
         let words = text.split(" ");
         let a = {}
+        let max = 0;
+        let maxword;
         for (let i = 0; i < words.length; i++) {
             a[words[i]] = (a[words[i]] || 0) + 1;
         }
-        let max = 0;
-        let maxword;
         for (let [key, val] of Object.entries(a)) {
             if (val > max) {
                 max = val;
                 maxword = key;
             }
-            // console.log(key, val);
         }
-        console.log(maxword);
         setText(maxword + " used " + max + " times.");
+        props.showAlert("Action Performed!", "success")
     }
 
     const handleCopy = () => {
-        var text = document.getElementById("myBox");
-        text.select();
-        navigator.clipboard.writeText(text.value);
+        navigator.clipboard.writeText(text);
         props.showAlert("text copied to clipboard", "success")
+    }
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/\s+/);
+        setText(newText.join(" "));
+        props.showAlert("ExtraSpaces Removed..", "success");
     }
 
     const handleOnChange = (event) => {
@@ -60,22 +59,35 @@ export default function TextForm(props) {
     return (
         <>
             <div className="container my-2" >
-                <h3 style={{ color: props.mode === 'light' ? 'black' : 'white' }}>{props.heading}</h3>
-                <div className="mb-3">
-                    <textarea className="form-control" value={text} style={{ backgroundColor: props.mode === 'light' ? 'dark' : 'light' }} onChange={handleOnChange} id="myBox" rows="10"></textarea>
+                <h3 style={{ color: props.mode === 'light' ? '#000000' : '#ffffff' }}>
+                    {props.heading}
+                </h3>
+                <div className="mb-2">
+                    <textarea className="form-control" value={text} style={{ backgroundColor: props.mode === 'light' ? 'dark' : 'light' }} onChange={handleOnChange} id="myBox" rows="8">
+                    </textarea>
                 </div>
-                <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
-                <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to Lowercase</button>
-                <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
-                <button className="btn btn-primary mx-1" onClick={handleMostFreqWord} >Most Freq. Word</button>
-                <button className="btn btn-primary mx-1" onClick={handleCopy} >Copy Text</button>
+                <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Captilize text</button>
+                <button className="btn btn-primary mx-1 my-1" onClick={handleLowClick}>Lowerize text</button>
+                <button className="btn btn-primary mx-1 my-1" onClick={handleMostFreqWord} >Most Freq. Word</button>
+                <button className="btn btn-primary mx-1 my-1" onClick={handleCopy} >Copy Text</button>
+                <button className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces} >Slice extra spces</button>
+                <button className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
             </div>
             <div className='container my-2'>
-                <h3 style={{ color: props.mode === 'light' ? 'black' : 'white' }}>Your text summary</h3>
-                <p style={{ color: props.mode === 'light' ? 'black' : 'white' }}>{text.split(" ").length} words & {text.length} chars</p>
-                <p style={{ color: props.mode === 'light' ? 'black' : 'white' }}>{0.008 * text.split(" ").length} Minutes to read</p>
-                <h4 style={{ color: props.mode === 'light' ? 'black' : 'white' }}>Preview</h4>
-                <p style={{ color: props.mode === 'light' ? 'black' : 'white' }}>{text.length > 0 ? text : "Enter something to preview"}</p>
+                <h5 style={{ color: props.mode === 'light' ? '#000000' : '#ffffff' }}>
+                    Your text summary
+                </h5>
+                <p style={{ color: props.mode === 'light' ? '#07091a' : '#7bf87f' }}>
+                    {text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} words, {text.length} chars
+                    <br />
+                    {0.008 * text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} min. to read
+                </p>
+                <h5 style={{ color: props.mode === 'light' ? '#000000' : '#ffffff' }}>
+                    Preview
+                </h5>
+                <p style={{ color: props.mode === 'light' ? '#07091a' : '#7bf87f' }}>
+                    {text.length > 0 ? text : "Nothing to preview"}
+                </p>
             </div>
         </>
     )
