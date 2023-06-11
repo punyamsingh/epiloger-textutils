39cad4fef76ec79d7572cc10edd3302f3c145126
punyamsingh <89277920+punyamsingh@users.noreply.github.com>
2023-06-11 16:31:44 +0530

    logo added to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,7 @@ export default function NavBar(props) {
     return (
         <nav className={`navbar navbar-expand-lg navbar-${props.mode === 'light' ? 'light' : 'dark'} bg-${props.mode}`}>
             <div className="container-fluid">
+                <img src="./logo192.png" alt="" width={40} height={38} style={{ margin: "0px 5px 0px 5px" }} />
                 <Link className="navbar-brand" to="/">{props.title}</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
